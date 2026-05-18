9a2b7d199770c236af507b08c9433a6732e26652
chiniu <kimpoychin131@gmail.com>
2026-05-18 19:25:10 +0800

    init commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import PomodoroTimer from "./components/PomodoroTimer/PomodoroTimer";
+
+
+function App(){
+    return(
+        <div className = "main">
+            <PomodoroTimer></PomodoroTimer>
+        </div>
+    );
+}
+
+export default App
---
+++ src/components/PomodoroTimer/PomodoroTimer.tsx
@@ -0,0 +1,11 @@
+import "./PomodoroTimer.css"
+
+function PomodoroTimer(){
+    return(
+        <div className = "pomodoro-timer">
+           
+        </div>
+    )    
+}
+
+export default PomodoroTimer
